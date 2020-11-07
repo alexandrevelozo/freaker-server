8546e07747124c4d4e9f8d50aff992410bbefe39
nicolasdev1 <nicolaspessoal@icloud.com>
2020-11-07 10:13:18 -0300

    fix: Resolve conflicts

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,20 @@
 module.exports = {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true
+  development: {
+    database: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      name: process.env.DB_NAME,
+      dialect: process.env.DB_DIALECT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    }
+  },
+  production: {
+    database: process.env.DATABASE_URL,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true
+    }
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
 import dotenv from 'dotenv'
+
 import express from 'express'
 import cors from 'cors'
 
 import routes from './routes'
 import './database/connection'
 
+dotenv.config()
+
 const app = express()
 
 app.use(cors())
 
-app.use(express.favicon())
-app.use(express.logger('dev'))
 app.use(express.json())
 app.use(routes)
-
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler())
-}
-
-dotenv.config()
 
 app.listen(3334, () =>
   console.log('Server running on port 3334...')
